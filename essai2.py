d7555b19ec06b39de982bc17b67fba64ce5aafbf
elia <eliarosina28@gmail.com>
2024-06-17 16:04:55 +1245

    My Django Girls app, first commit

---
+++ essai2.py
@@ -0,0 +1,10 @@
+admin="organisateurpython"
+print(admin)
+print("pop")
+print("name")
+print()
+
+if(5>2) :
+    print(False)
+else  :
+    print(True)
---
+++ test1.py
@@ -0,0 +1,10 @@
+print (1+2) 
+print (2*40) 
+print("bora"*2)
+
+print("salut")
+"salut" + "XJ5YJYRHRHRHR"
+len("azertyqsdfg")
+print (len("gelato"))
+print (name="elia")
+print (partipant ='name':'')
